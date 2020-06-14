208d182f0633c3af00fc353d542fdae3da0fed09
Brian Davis <brian.davis@ciphertrace.com>
2020-06-14 19:20:59 -0400

    move tests to use mock if no external network connection exists

--- app/tests.py
+++ app/tests.py
@@ -1,9 +1,18 @@
 import app
+from unittest.mock import Mock, patch
 
 # test response guage and expect 200
 def test_pass_response():
-    assert app.response_guage('https://httpstat.us/200') == 200
+    with patch('requests.get') as mock_request:
+        url = 'https://httpstat.us/200'
+        mock_request.return_value.status_code = 200
+
+        assert app.response_guage(url) == 200
 
 # test response guage and expect 503
 def test_fail_response():
-    assert app.response_guage('https://httpstat.us/503') == 503
+    with patch('requests.get') as mock_request:
+        url = 'https://httpstat.us/503'
+        mock_request.return_value.status_code = 503
+
+        assert app.response_guage(url) == 503
